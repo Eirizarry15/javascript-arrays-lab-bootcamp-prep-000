ce03f17403eb82ebefc8a85864f81b0bd37fc645
Edgar Irizarry <eirizarry15@yahoo.com>
2018-09-13 19:46:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 // Add your functions and code here
 
-var kittens = ["Ralph", "Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(name) {
-  kittens.push(name)
-  return kittens
-}
-
-function destructivelyAppendKitten(name){
-kittens.unshift(name)
-return kittens
-}
 
